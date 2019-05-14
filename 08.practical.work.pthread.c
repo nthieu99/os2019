03857cfc7c42aca1d67f879e3d0b99964a9a1dd3
nthieu99 <hieunt.b8067@st.usth.edu.vn>
2019-05-14 09:35:06 +0700

    08.practical

--- 08.practical.work.pthread.c
+++ 08.practical.work.pthread.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #define BUFFER_SIZE 10
 
 typedef struct {
@@ -14,7 +15,7 @@ int first = 0;
 int last = 0;
 
 void printLog(){
-  printf("first = %d\t last = %d\n", first, last);
+  printf("First = %d\t last = %d\n", first, last);
   return;
 }
 
@@ -46,12 +47,31 @@ item* initItem(char type, int amount, char unit){
   return i;
 }
 
-int main(){
-  item* order1 = initItem('1',3,'0');
-  item* order2 = initItem('0',9,'1');
-  printf("Initial log:\nfirst = %d\tlast = %d\n\n", first, last);
-  produce(order1);
-  produce(order2);
+void *produceThread(void *param){
+  printf("This is produce thread\n");
+  produce(initItem('1',3,'0'));
+  produce(initItem('0',9,'1'));
+  produce(initItem('0',3,'0'));
+}
+
+void *consumeThread(void *param){
+  printf("This is consume thread\n");
   consume();
+  consume();
+}
+
+int main(){
+  pthread_t tid1, tid2;
+  printf("Initial log:\nfirst = %d\tlast = %d\n\n", first, last);
+
+    if(pthread_create(&tid1, NULL, produceThread, NULL) != 0){
+    perror("pthread_created() error");
+  }
+  if(pthread_create(&tid2, NULL, consumeThread, NULL) != 0){
+    perror("pthread_created() error");
+  }
+  
+  pthread_join(tid1, NULL);
+  pthread_join(tid2, NULL);
   return 0;
 }
